01887f0e8a84dd7c09564617a0aff9de6046f9ab
Ruidong Wu <mooninrain@outlook.com>
2020-02-25 14:36:32 -0500

    Update dict_gather.py

--- jactorch/parallel/dict_gather.py
+++ jactorch/parallel/dict_gather.py
@@ -49,6 +49,7 @@ def dict_gather(outputs, target_device, dim=0):
         if isinstance(out, Variable) or torch.is_tensor(out):
             if out.dim() == 0:
                 outputs = [o.unsqueeze(0) for o in outputs]
+            import pdb; pdb.set_trace()
             return Gather.apply(target_device, dim, *outputs)
         elif out is None:
             return None
